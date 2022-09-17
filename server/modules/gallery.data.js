a461f0385d1ded7f16fcada1339cf87c8a77c0f8
Yanira Sanchez <yani@Yaniras-MacBook-Pro.local>
2022-09-17 14:02:47 -0500

    added and tested GET request to app.jsx

--- server/modules/gallery.data.js
+++ server/modules/gallery.data.js
@@ -1,12 +1,12 @@
 const galleryItems = [
-    { id: 1, path: 'images/coast.jpg', description: 'Photo of Foggy day off the coast of New Zealand.', likes: 0 },
-    { id: 2, path: 'images/deer.jpg', description: 'Photo of a dear taken at Glacier National Park.', likes: 0 },
-    { id: 3, path: 'images/forest.jpg', description: 'Photo of the woods during fall season taken at Glacier National Park.', likes: 0 },
-    { id: 4, path: 'images/forest2.jpg', description: 'Photo of the forest during a foggy morning hike.', likes: 0 },
-    { id: 5, path: 'images/lama.jpg', description: 'Photo of a lama taken at Glacier National Park.', likes: 0 },
-    { id: 6, path: 'images/sun.jpg', description: 'Photo of a beautiful red sun.', likes: 0 },
-    { id: 7, path: 'images/sunflower.jpg', description: 'Photo of some beautiful sunflowers.', likes: 0 },
-    { id: 8, path: 'images/winterForest.jpg', description: 'Photo taken from a helicopter ride in Washington State.', likes: 0 }
+    { id: 1, path: '../images/coast.jpg', description: 'Photo of Foggy day off the coast of New Zealand.', likes: 0 },
+    { id: 2, path: '../images/deer.jpg', description: 'Photo of a dear taken at Glacier National Park.', likes: 0 },
+    { id: 3, path: '../images/forest.jpg', description: 'Photo of the woods during fall season taken at Glacier National Park.', likes: 0 },
+    { id: 4, path: '../images/forest2.jpg', description: 'Photo of the forest during a foggy morning hike.', likes: 0 },
+    { id: 5, path: '../images/lama.jpg', description: 'Photo of a lama taken at Glacier National Park.', likes: 0 },
+    { id: 6, path: '../images/sun.jpg', description: 'Photo of a beautiful red sun.', likes: 0 },
+    { id: 7, path: '../images/sunflower.jpg', description: 'Photo of some beautiful sunflowers.', likes: 0 },
+    { id: 8, path: '../images/winterForest.jpg', description: 'Photo taken from a helicopter ride in Washington State.', likes: 0 }
 ];
 
 module.exports = galleryItems;
