0e1e51bca16405eb303510c88c602872a2a6c4fe
Yanira Sanchez <yani@Yaniras-MacBook-Pro.local>
2022-09-17 16:48:02 -0500

    added GalleryItem function and tested PUT request when like button is clicked

--- src/components/GalleryItem/GalleryItem.jsx
+++ src/components/GalleryItem/GalleryItem.jsx
@@ -1,6 +1,30 @@
-// imports each gallery item
+import axios from 'axios';
 
-export default function GalleryItem({galleryItem}){
 
+// imports each gallery object and creates a gallery item with a like button and text with number of likes displayed. 
+// {id: 2, path: '../images/deer.jpg', description: 'Photo of a dear taken at Glacier National Park.', likes: 0}
+
+export default function GalleryItem({galleryItem, fetchItems}){
+    // console.log(galleryItem);
+    const updateLikes = ()=> {
+        // send PUT request to server to update the number of likes. The server side updates the count.
+        // console.log('click');
+        axios({
+            method: 'PUT',
+            url: `/gallery/like/${galleryItem.id}`
+        }).then((response)=> {
+            console.log('put request is all good', response);
+        }).catch((error)=> {
+
+        })
+    }
+
+    return(
+        <div>
+            <img src={galleryItem.path} alt={galleryItem.description} />
+            <p>test</p>
+            <button onClick={updateLikes}>Like</button>
+        </div>
+    );
 
 }
--- src/components/GalleryList/GalleryList.jsx
+++ src/components/GalleryList/GalleryList.jsx
@@ -1,14 +1,18 @@
+import GalleryItem from "../GalleryItem/GalleryItem.jsx"
 
 // import galleryList from App.jsx which contains an array of all of the gallery objects:
 // example data: [{id: 2, path: '../images/deer.jpg', description: 'Photo of a dear taken at Glacier National Park.', likes: 0}...]
-export default function GalleryList({galleryList}){
+
+export default function GalleryList({galleryList, fetchItems}){
+    // console.log(galleryList);
     return(
         <div>
             {galleryList.map( galleryItem => {
                 return(
-                    <GalleryItem galleryItem = {galleryItem}/>
-                )
-            })}
+                    <GalleryItem galleryItem = {galleryItem} fetchItems={fetchItems}/>
+                    )
+                })}
         </div>
     )
 }
+{/* <GalleryItem galleryItem = {galleryItem}/> */}
